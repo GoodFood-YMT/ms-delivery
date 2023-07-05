f1e538e980c3b8194da9a1dad350dafa0f883826
tom-brulin <t.brulin@outlook.com>
2023-07-05 10:50:24 +0200

    feat(core): error handling

--- app/Controllers/Http/AddressesController.ts
+++ app/Controllers/Http/AddressesController.ts
@@ -5,120 +5,155 @@ import UpdateAddressValidator from 'App/Validators/UpdateAddressValidator'
 
 export default class AddressesController {
   public async index({ request }: HttpContextContract) {
-    const userId = request.header('UserID')
+    try {
+      const userId = request.header('UserID')
 
-    if (!userId) {
-      throw new Error('User not found')
-    }
+      if (!userId) {
+        throw new Error('User not found')
+      }
 
-    const addresses = await Address.query().where('user_id', userId)
+      const addresses = await Address.query().where('user_id', userId)
 
-    return {
-      addresses,
+      return {
+        addresses,
+      }
+    } catch (error) {
+      console.log(error)
+      return {
+        error: error.message,
+      }
     }
   }
 
   public async store({ request }: HttpContextContract) {
-    const userId = request.header('UserID')
+    try {
+      const userId = request.header('UserID')
 
-    if (!userId) {
-      throw new Error('User not found')
-    }
+      if (!userId) {
+        throw new Error('User not found')
+      }
 
-    const payload = request.validate(CreateAddressValidator)
+      const payload = request.validate(CreateAddressValidator)
 
-    const address = await Address.create({
-      ...payload,
-      userId,
-    })
+      const address = await Address.create({
+        ...payload,
+        userId,
+      })
 
-    return {
-      ...address,
+      return {
+        ...address,
+      }
+    } catch (error) {
+      console.log(error)
+      return {
+        error: error.message,
+      }
     }
   }
 
   public async show({ request }: HttpContextContract) {
-    const userId = request.header('UserID')
+    try {
+      const userId = request.header('UserID')
 
-    if (!userId) {
-      throw new Error('User not found')
-    }
+      if (!userId) {
+        throw new Error('User not found')
+      }
 
-    const addressId = request.param('id')
+      const addressId = request.param('id')
 
-    const address = await Address.query().where('user_id', userId).where('id', addressId).first()
+      const address = await Address.query().where('user_id', userId).where('id', addressId).first()
 
-    if (!address) {
-      throw new Error('Address not found')
-    }
+      if (!address) {
+        throw new Error('Address not found')
+      }
 
-    return {
-      ...address,
+      return {
+        ...address,
+      }
+    } catch (error) {
+      console.log(error)
+      return {
+        error: error.message,
+      }
     }
   }
 
   public async update({ request }: HttpContextContract) {
-    const userId = request.header('UserID')
+    try {
+      const userId = request.header('UserID')
 
-    if (!userId) {
-      throw new Error('User not found')
-    }
+      if (!userId) {
+        throw new Error('User not found')
+      }
 
-    const payload = await request.validate(UpdateAddressValidator)
-    const addressId = request.param('id')
+      const payload = await request.validate(UpdateAddressValidator)
+      const addressId = request.param('id')
 
-    const address = await Address.query().where('user_id', userId).where('id', addressId).first()
+      const address = await Address.query().where('user_id', userId).where('id', addressId).first()
 
-    if (!address) {
-      throw new Error('Address not found')
-    }
+      if (!address) {
+        throw new Error('Address not found')
+      }
 
-    if (payload.name) {
-      address.name = payload.name
-    }
+      if (payload.name) {
+        address.name = payload.name
+      }
 
-    if (payload.street) {
-      address.street = payload.street
-    }
+      if (payload.street) {
+        address.street = payload.street
+      }
 
-    if (payload.zipCode) {
-      address.zipCode = payload.zipCode
-    }
+      if (payload.zipCode) {
+        address.zipCode = payload.zipCode
+      }
 
-    if (payload.city) {
-      address.city = payload.city
-    }
+      if (payload.city) {
+        address.city = payload.city
+      }
 
-    if (payload.country) {
-      address.country = payload.country
-    }
+      if (payload.country) {
+        address.country = payload.country
+      }
 
-    await address.save()
+      await address.save()
 
-    return {
-      ...address,
+      return {
+        ...address,
+      }
+    } catch (error) {
+      console.log(error)
+      return {
+        error: error.message,
+      }
     }
   }
 
   public async destroy({ request }: HttpContextContract) {
-    const userId = request.header('UserID')
+    try {
+      const userId = request.header('UserID')
 
-    if (!userId) {
-      throw new Error('User not found')
-    }
+      if (!userId) {
+        throw new Error('User not found')
+      }
 
-    const addressId = request.param('id')
+      const addressId = request.param('id')
 
-    const address = await Address.query().where('user_id', userId).where('id', addressId).first()
+      const address = await Address.query().where('user_id', userId).where('id', addressId).first()
 
-    if (!address) {
-      throw new Error('Address not found')
-    }
+      if (!address) {
+        throw new Error('Address not found')
+      }
 
-    await address.delete()
+      await address.delete()
 
-    return {
-      success: true,
+      return {
+        success: true,
+      }
+    } catch (error) {
+      console.log(error)
+      return {
+        error: error.message,
+      }
     }
   }
 }
--- app/Controllers/Http/DeliveriesController.ts
+++ app/Controllers/Http/DeliveriesController.ts
@@ -5,46 +5,67 @@ import UpdateDeliveryValidator from 'App/Validators/UpdateDeliveryValidator'
 
 export default class DeliveriesController {
   public async store({ request }: HttpContextContract) {
-    const payload = await request.validate(CreateDeliveryValidator)
+    try {
+      const payload = await request.validate(CreateDeliveryValidator)
 
-    const delivery = await Delivery.create({
-      ...payload,
-    })
+      const delivery = await Delivery.create({
+        ...payload,
+      })
 
-    return {
-      ...delivery,
+      return {
+        ...delivery,
+      }
+    } catch (error) {
+      console.log(error)
+      return {
+        error: error.message,
+      }
     }
   }
 
   public async show({ request }: HttpContextContract) {
-    const deliveryId = request.param('id')
+    try {
+      const deliveryId = request.param('id')
 
-    const delivery = await Delivery.find(deliveryId)
+      const delivery = await Delivery.find(deliveryId)
 
-    if (!delivery) {
-      throw new Error('Delivery not found')
-    }
+      if (!delivery) {
+        throw new Error('Delivery not found')
+      }
 
-    return {
-      ...delivery,
+      return {
+        ...delivery,
+      }
+    } catch (error) {
+      console.log(error)
+      return {
+        error: error.message,
+      }
     }
   }
 
   public async update({ request }: HttpContextContract) {
-    const deliveryId = request.param('id')
-    const payload = await request.validate(UpdateDeliveryValidator)
+    try {
+      const deliveryId = request.param('id')
+      const payload = await request.validate(UpdateDeliveryValidator)
 
-    const delivery = await Delivery.find(deliveryId)
+      const delivery = await Delivery.find(deliveryId)
 
-    if (!delivery) {
-      throw new Error('Delivery not found')
-    }
+      if (!delivery) {
+        throw new Error('Delivery not found')
+      }
 
-    delivery.status = payload.status
-    await delivery.save()
+      delivery.status = payload.status
+      await delivery.save()
 
-    return {
-      ...delivery,
+      return {
+        ...delivery,
+      }
+    } catch (error) {
+      console.log(error)
+      return {
+        error: error.message,
+      }
     }
   }
 }
