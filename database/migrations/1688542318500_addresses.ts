af2d8c1de9d4e4b1cd7d0274d19f9c98d49689ce
tom-brulin <t.brulin@outlook.com>
2024-01-12 09:40:28 +0100

    feat: change lat lon to float

--- database/migrations/1688542318500_addresses.ts
+++ database/migrations/1688542318500_addresses.ts
@@ -13,8 +13,8 @@ export default class extends BaseSchema {
       table.string('city').notNullable()
       table.string('country').notNullable()
 
-      table.decimal('lat').notNullable()
-      table.decimal('lon').notNullable()
+      table.float('lat').notNullable()
+      table.float('lon').notNullable()
 
       table.string('user_id').notNullable()
     })
